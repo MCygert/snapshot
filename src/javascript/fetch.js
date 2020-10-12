45c44422bce354495ecad0a20ba4ecdf1e12e5d6
Mikolaj Cygert <miki@comsite.com.pl>
2020-10-12 23:57:17 +0200

    Change fecthing

--- src/javascript/fetch.js
+++ src/javascript/fetch.js
@@ -1,19 +1,21 @@
-import {
-    createClient
-} from "pexels";
+class PhotoGallery {
+  constructor() {
+    const api_key = "563492ad6f917000010000016a1cddb5e2924a85a77ad1d190cb3915";
+    this.galleryDiv = document.querySelector("ul");
+    this.images = this.getImage();
+  }
 
-const api_key = "563492ad6f917000010000016a1cddb5e2924a85a77ad1d190cb3915";
-const client = createClient(api_key);
-const query = "Nature";
-console.log("it's working");
-client.photos.random().then((photo) => {
-    console.log(photo);
-});
-client.photos
-    .search({
-        query,
-        per_page: 1,
-    })
-    .then((photos) => {
-        console.log(photos);
+  async getImage() {
+    const baseURL = "https://api.pexels.com/v1/search?query=nature&per_page=1";
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: this.api_key,
+      },
     });
+    const data = await response.json();
+    console.log(data);
+  }
+}
+const photo = new PhotoGallery();
