d1bc863e9ff0f79a5151071f92ef9656225ffca8
Mikolaj Cygert <cygertmikolaj@gmail.com>
2020-10-15 00:19:43 +0200

    Change fetching

--- src/javascript/fetch.js
+++ src/javascript/fetch.js
@@ -1,21 +1,13 @@
-class PhotoGallery {
-  constructor() {
-    const api_key = "563492ad6f917000010000016a1cddb5e2924a85a77ad1d190cb3915";
-    this.galleryDiv = document.querySelector("ul");
-    this.images = this.getImage();
-  }
+import fetch from "node-fetch"
+import Unsplash, { toJson } from 'unsplash-js';
 
-  async getImage() {
-    const baseURL = "https://api.pexels.com/v1/search?query=nature&per_page=1";
-    const response = await fetch(baseURL, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: this.api_key,
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-  }
-}
-const photo = new PhotoGallery();
+global.fetch = fetch;
+
+const API_KEY = "fyF0hAswvNUJVVIFch90U0H6zsM6TlcOGLc0sQiX9mA";
+const unsplash = new Unsplash({ accessKey: API_KEY });
+
+unsplash.search.collections("dogs", 1)
+.then(toJson)  
+.then(json => {
+    console.log(json);
+});
