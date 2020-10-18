8887fa6d1ec1fe9d5bdcd95cb28d0ffbfafa57d3
Mikolaj Cygert <cygertmikolaj@gmail.com>
2020-10-18 14:42:49 +0200

    add searching for photos

--- src/javascript/fetch.js
+++ src/javascript/fetch.js
@@ -1,13 +1,25 @@
-import fetch from "node-fetch"
-import Unsplash, { toJson } from 'unsplash-js';
-
-global.fetch = fetch;
-
 const API_KEY = "fyF0hAswvNUJVVIFch90U0H6zsM6TlcOGLc0sQiX9mA";
-const unsplash = new Unsplash({ accessKey: API_KEY });
 
-unsplash.search.collections("dogs", 1)
-.then(toJson)  
-.then(json => {
-    console.log(json);
-});
+$.get("https://api.unsplash.com/photos/?client_id=" + API_KEY, function (data) {
+  var $galleryContainer = $('.photo-container');
+
+  console.log(data);
+  data.map(photo => {
+    $galleryContainer.append('<div class="photo"><img src="' + photo.urls.small + '"></div>');
+  });
+}, );
+
+function getPhotos() {
+  let query = $('input').val();
+  $.get("https://api.unsplash.com/search/photos/?client_id=" + API_KEY + "&query=" + query, function (data) {
+    var $galleryContainer = $('.photo-container');
+    $galleryContainer.empty();
+
+    console.log(data);
+    data.results.map(photo => {
+      console.log(photo);
+      $galleryContainer.append('<div class="photo"><img src="' + photo.urls.small + '"></div>')
+    });
+  }, );
+  return false;
+};
